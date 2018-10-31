efe9fa2891957e37284e6c130cb3359d456e414b
Shide Foo <shide.foo@sap.com>
2018-10-31 18:11:54 +0800

    model training using full corpus trained iteratively completed

--- model_trainer.py
+++ model_trainer.py
@@ -7,22 +7,31 @@ import pickle
 base_dir = os.path.dirname(os.path.realpath(__file__))
 dictionary_fn = os.path.join(base_dir, 'dictionary.complete')
 bow_parts = os.path.join(base_dir, 'bow_parts')
+
+dictionary = gensim.corpora.Dictionary().load(dictionary_fn)
 fn_prefix = 'bow_corpus_complete'
 postfix = 'abcdefghijklmno'
-def build_bow_corpus():
-	bow_corpus = []
+lda_model = LdaMulticore(num_topics=250, id2word=dictionary, passes=2, workers=3)
 
-	def _extract_bow_part(postfix):
+
+def build_and_train_lda_model(lda_model, fn_prefix, postfix):
+	bow_corpus = []
+	print("building corpus")
+	def _extract_bow_part(postfix, fn_prefix):
 		for letter in postfix:
-			fname = '{}.{}.pkl'.format(fn_prefix, letter)
+			print("extracting {}".format(letter))
+			fname = os.path.join(bow_parts, '{}.{}.pkl'.format(fn_prefix, letter))
 			with open(fname, 'rb') as file:
 				bow_part = pickle.load(file)
 			yield bow_part
-	for bow_part in _extract_Bow_part(postfix):
-		bow_corpus += bow_part
 
-	return bow_corpus
+	for bow_part in _extract_bow_part(postfix, fn_prefix):
+		print("updating lda model")
+		lda_model.update(bow_part)
+	print("model training complete")
 
-dictionary = gensim.corpora.Dictionary().load(dictionary_fn)
-lda_model = LdaMulticore(bow_corpus, num_topics=300, id2word=dictionary, passes=2, workers=8)
-lda_model.save('lda_complete.model')
+
+print("training model")
+build_and_train_lda_model(lda_model, fn_prefix, postfix)
+lda_model.save('model_complete/lda_complete.model')
+print("lda model trained and saved")
